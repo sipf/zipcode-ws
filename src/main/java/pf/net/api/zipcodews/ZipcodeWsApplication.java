bbc90eecc4a1cc8a6a87a1e0c9cc642811c1595e
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2017-10-05 17:16:17 -1000

    Enable caching

--- src/main/java/pf/net/api/zipcodews/ZipcodeWsApplication.java
+++ src/main/java/pf/net/api/zipcodews/ZipcodeWsApplication.java
@@ -2,8 +2,10 @@ package pf.net.api.zipcodews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ZipcodeWsApplication {
 
 	public static void main(String[] args) {
